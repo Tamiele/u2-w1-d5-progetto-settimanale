d67b669e210c27ce4951c7824725461ff78253da
Tamiele <tammaro_85@hotmail.it>
2024-09-13 16:46:12 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let header = document.querySelector("header");
+let button = document.querySelector("button");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 240) {
+    header.classList.add("cheangeColorHeader");
+    button.classList.add("cheangeColorButton");
+  } else {
+    header.classList.remove("cheangeColorHeader");
+    button.classList.remove("cheangeColorButton");
+  }
+});
+
+let allGroups = document.querySelectorAll("svg g");
+
+const prova = function () {
+  allGroups.forEach((group, index) => {
+    let opacityValue = group.getAttribute("opacity");
+
+    if (opacityValue === "0") {
+      setInterval(() => {
+        group.setAttribute("opacity", "1");
+      }, index * 1);
+    } else {
+      group.setAttribute("opacity", "1");
+    }
+  });
+};
+
+window.onload = function () {
+  prova();
+};
